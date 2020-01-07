58a31ccc873f8210a5032a267371815a5bafc34f
Valter Ferreira Lopes Neto <valter.neto@gft.com>
2020-01-07 16:28:49 -0300

    reescrevendo componente Title com pure functions

--- src/title.js
+++ src/title.js
@@ -2,6 +2,18 @@
 
 import React from 'react'
 
+const Title = ({ nome, sobrenome }) => (
+    <h1>Olá { `${nome} ${sobrenome.first} ${sobrenome.last}`}!</h1>
+)
+
+Title.defaultProps = {
+    nome: 'Desconhecido',
+    sobrenome: { first: 'Sobrenome ',
+                    last: 'não informado'
+                }
+}
+
+/*
 const Title = React.createClass({
     getDefaultProps: function () {
         return {
@@ -16,6 +28,6 @@ const Title = React.createClass({
                 <h1>Olá {this.props.nome + ' ' + this.props.sobrenome.first + ' ' + this.props.sobrenome.last}!</h1>
             )
         }
-    })
+    })*/
 
 export default Title
