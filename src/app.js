fe3ce6b119bc7a904832e7f24b64d496e7a52f67
Valter Ferreira Lopes Neto <valter.neto@gft.com>
2020-01-07 17:34:21 -0300

    Loops e keys

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@
 
 import React, { Component }from 'react'
 import Title from "./title";
+import Square from "./square";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Title nome='Jhonnis' sobrenome={ { first: 'Da', last: 'Silva'} }/>
+
+                {['blue', 'yellow', 'green'].map( ( color, index ) => (
+                    <Square key={ index } color={ color }/>
+                ))}
             </div>
         )
     }
